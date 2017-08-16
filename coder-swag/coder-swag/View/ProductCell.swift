d43939ac307761db35af2e5f8e27d3cf9f60a173
Phuc Le Dien <phucledien@gmail.com>
2017-08-17 00:20:16 +0700

    Added UICollectionView

---
+++ coder-swag/coder-swag/View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Phuc Le Dien on 8/17/17.
+//  Copyright © 2017 Phuc Le Dien. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
